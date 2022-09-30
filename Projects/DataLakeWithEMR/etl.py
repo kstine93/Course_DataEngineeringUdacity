94be09bf9f04b7945068bfa73377cde235c2e035
Kevin Stine <kstine93@gmail.com>
2022-09-30 09:08:55 +0200

    Additional tweaks and improvements to etl.py for data lake project

--- Projects/DataLakeWithEMR/etl.py
+++ Projects/DataLakeWithEMR/etl.py
@@ -1,33 +1,36 @@
 import configparser
-from datetime import datetime
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col, expr
-from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format, desc
+from pyspark.sql import functions as F
 from pyspark.sql.types import IntegerType, StringType, FloatType, StructType, StructField
 
-
+#<FOR TESTING>
 aws_path = "/home/rambino/.aws/credentials"
 aws_cred = configparser.ConfigParser()
 aws_cred.read(aws_path)
-
-#FOR TESTING
 os.environ['AWS_ACCESS_KEY_ID'] = aws_cred['default']['aws_access_key_id']
 os.environ['AWS_SECRET_ACCESS_KEY'] = aws_cred['default']['aws_secret_access_key']
+#</FOR TESTING>
 
-
-def create_spark_session():
-    spark = SparkSession \
+def create_S3_spark_session():
+    """Wrapper for building a spark session with pre-set configuration
+    
+    Returns:
+        SparkSession: spark session for connecting with AWS S3
+    """
+    return SparkSession \
         .builder \
         .config("spark.jars.packages","com.amazonaws:aws-java-sdk-s3:1.12.311") \
         .config("spark.jars.packages","org.apache.hadoop:hadoop-aws:3.3.4") \
         .getOrCreate()
 
-    return spark
-
 def get_log_schema():
-
-        return StructType([
+    """Wrapper for returning schema for log data.
+        
+    Returns:
+        StructType: schema of songs data to be loaded in.
+    """
+    return StructType([
         StructField('artist',StringType(),True),
         StructField('auth',StringType(),True),
         StructField('firstName',StringType(),True),
@@ -49,8 +52,12 @@ def get_log_schema():
     ])
 
 def get_song_schema():
-
-        return StructType([
+    """Simple wrapper for returning schema for song data.
+    
+    Returns:
+        StructType: schema of songs data to be loaded in.
+    """
+    return StructType([
         StructField('num_songs',IntegerType(),True),
         StructField('artist_id',StringType(),True),
         StructField('artist_latitude',FloatType(),True),
@@ -64,6 +71,15 @@ def get_song_schema():
     ])
 
 def process_songplays(spark, song_df, log_df, output_path_prefix):
+    """Joins song and log data to create songplays dataframe. Writes to output.
+
+    Args:
+        spark (SparkSession): current spark session to be used in joining, writing data
+        song_df (DataFrame): Spark df holding song data.
+        log_df (DataFrame): Spark df holding log data. Pre-filtered for 'page=NextSong'
+        output_path_prefix (string): base filepath to write data to.
+    """
+
     # extract columns from joined song and log datasets to create songplays table 
     match_condition = [log_df.song == song_df.title, log_df.artist == song_df.artist_name]
 
@@ -74,7 +90,7 @@ def process_songplays(spark, song_df, log_df, output_path_prefix):
             song_df.song_id, song_df.artist_id
         )
 
-    songplays_df = songplays_df.withColumn('songplay_id',expr("uuid()"))
+    songplays_df = songplays_df.withColumn('songplay_id',F.expr("uuid()"))
 
     songplays_df \
         .select("songplay_id","ts","userId","level","song_id","artist_id","sessionId","location","userAgent", "year","month") \
@@ -85,6 +101,18 @@ def process_songplays(spark, song_df, log_df, output_path_prefix):
 
 
 def process_song_data(spark, song_schema, input_paths, output_path_prefix):
+    """Reads song data from input path, writes 'songs' and 'artists' tables. returns dataframe
+    for further processing.
+
+    Args:
+        spark (SparkSession): current spark session to be used in joining, writing data
+        song_schema (StructType): shows schema of song data. Used in data read.
+        input_paths: (dict): lookup dict for pre-set paths to read data from.
+        output_path_prefix (string): base filepath to write data to.
+
+    Returns:
+        DataFrame: song data to be used in further processing
+    """
 
     # read song data file
     song_df = spark \
@@ -96,25 +124,43 @@ def process_song_data(spark, song_schema, input_paths, output_path_prefix):
     # extract columns to create songs table
     songs_table = song_df.select('song_id','title','artist_id','year','duration')
     
+    # filter out duplicate songs
+    songs_table = songs_table.dropDuplicates(['song_id'])
+
     # write songs table to csv files partitioned by year and artist
     songs_table.write \
-    .option("header",True) \
-    .partitionBy("year","artist_id") \
-    .csv(output_path_prefix + "songs")
+        .option("header",True) \
+        .partitionBy("year","artist_id") \
+        .csv(output_path_prefix + "songs")
 
     # extract columns to create artists table
     artists_table = song_df.select('artist_id','artist_name','artist_location','artist_latitude','artist_longitude')
 
+    # filter out duplicate songs
+    artists_table = artists_table.dropDuplicates(['artist_id'])
+
     # write artists table to csv files
     artists_table.write \
-    .option("header",True) \
-    .partitionBy("artist_id") \
-    .csv(output_path_prefix + "artists")
+        .option("header",True) \
+        .partitionBy("artist_id") \
+        .csv(output_path_prefix + "artists")
 
     return song_df
 
 
 def process_log_data(spark, log_schema, input_paths, output_path_prefix):
+    """Reads log data from input path, writes 'users' and 'time' tables. returns dataframe
+    for further processing.
+
+    Args:
+        spark (SparkSession): current spark session to be used in joining, writing data
+        log_schema (StructType): shows schema of log data. Used in data read.
+        input_paths: (dict): lookup dict for pre-set paths to read data from.
+        output_path_prefix (string): base filepath to write data to.
+
+    Returns:
+        DataFrame: log data to be used in further processing
+    """
 
     # read log data file
     log_df = spark \
@@ -124,14 +170,14 @@ def process_log_data(spark, log_schema, input_paths, output_path_prefix):
         .load(input_paths['logs_raw'])
 
     # filter by actions for song plays
-    #log_df = df.filter() #Test in notebook
+    log_df = log_df.filter("page = 'nextSong'")
 
     # extract columns for users table - filtering out duplicates
     users_table = log_df.select('userId','firstName','lastName','gender','level')
 
     # filter out duplicate users - preferring more recent entries
     users_table = users_table \
-        .orderBy(desc('ts')) \
+        .orderBy(F.desc('ts')) \
         .dropDuplicates(['userId'])
     
     # write users table to csv files
@@ -140,30 +186,15 @@ def process_log_data(spark, log_schema, input_paths, output_path_prefix):
     .option("header",True) \
     .csv(output_path_prefix + "users")
 
-    #NOTE: I don't know why I would need this function.. not necessary for getting month, etc.
-    # create timestamp column from original timestamp column
-    #get_timestamp = udf()
-    #df_time = log_df.withColumn('timestamp',get_timestamp('ts'))
-    
-    # create datetime column from original timestamp column
-    get_datetime = udf(lambda x: datetime.fromtimestamp(x/1000))
-    log_df = log_df.withColumn('timestamp',get_datetime('ts'))
-
-    # more udfs to extract specific parts of date from datetime
-    get_hour = udf(lambda x: x.hour)
-    get_day = udf(lambda x: x.day)
-    get_week = udf(lambda x: x.isocalendar().week)
-    get_month = udf(lambda x: x.month)
-    get_year = udf(lambda x: x.year)
-    get_weekday = udf(lambda x: x.weekday())
-    
+    #Parsing epoch time to create date & time information
     log_df = log_df \
-        .withColumn('hour',get_hour('timestamp')) \
-        .withColumn('day',get_day('timestamp')) \
-        .withColumn('week',get_week('timestamp')) \
-        .withColumn('month',get_month('timestamp')) \
-        .withColumn('year',get_year('timestamp')) \
-        .withColumn('weekday',get_weekday('timestamp'))
+        .withColumn('date',F.to_timestamp(log_df['ts']/1000)) \
+        .withColumn('hour',F.hour('date')) \
+        .withColumn('day',F.dayofmonth('date')) \
+        .withColumn('week',F.weekofyear('date')) \
+        .withColumn('month',F.month('date')) \
+        .withColumn('year',F.year('date')) \
+        .withColumn('weekday',F.dayofweek('date')) \
 
     # extract columns to create time table
     time_table = log_df.select('ts','hour','day','week','month','year','weekday')
@@ -178,7 +209,9 @@ def process_log_data(spark, log_schema, input_paths, output_path_prefix):
 
 
 def main():
-    spark = create_spark_session()
+    """ETL application for processing Sparkify log and song data and producing data tables in S3"""
+    
+    spark = create_S3_spark_session()
 
     output_path_prefix = "./_out/"#"s3a://rambino-output/"
 
